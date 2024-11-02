88542ff3305c2a22ed97d7bc1b05f5a2e95c0b05
moriame01 <ir.era.eries@gmail.com>
2024-11-02 20:33:08 +0200

    3

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -14,6 +14,7 @@ public class Player extends Entity{
 
     public final int screenX;
     public final int screenY;
+    int hasKey = 0;
 
 
     public Player(GamePanel gp, KeyHandler keyH){
@@ -26,6 +27,8 @@ public class Player extends Entity{
         solidArea = new Rectangle();
         solidArea.x = 8;
         solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
         solidArea.width = 32;
         solidArea.height = 32;
 
@@ -69,6 +72,8 @@ public class Player extends Entity{
             collisionOn = false;
             gp.cChecker.checkTile(this);
 
+            int objIndex = gp.cChecker.checkObject(this, true);
+            pickUpObject(objIndex);
             if(collisionOn == false){
                 switch(direction){
                     case "up": worldY -= speed; break;
@@ -90,6 +95,29 @@ public class Player extends Entity{
         }
 
     }
+
+    public void pickUpObject(int i){
+        if(i != 999){
+            String objectName = gp.obj[i].name;
+
+            switch(objectName){
+                case "Key":
+                    hasKey++;
+                    gp.obj[i] = null;
+                    System.out.println("Key: "+hasKey);
+                    break;
+                case "Door":
+                    if(hasKey > 0){
+                        gp.obj[i] = null;
+                        hasKey--;
+                    }
+                    System.out.println("Key: "+hasKey);
+
+                    break;
+            }
+        }
+    }
+
     public void draw(Graphics2D g2){
 //        g2.setColor(Color.white);
 //        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
