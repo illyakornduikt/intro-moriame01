8f449516aa3ff3258b8c81566ba82e65cbe1f329
moriame01 <ir.era.eries@gmail.com>
2024-12-05 20:55:09 +0200

    9,10

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -14,7 +14,7 @@ public class Player extends Entity{
 
     public final int screenX;
     public final int screenY;
-    int hasKey = 0;
+    public int hasKey = 0;
 
 
     public Player(GamePanel gp, KeyHandler keyH){
@@ -102,18 +102,36 @@ public class Player extends Entity{
 
             switch(objectName){
                 case "Key":
+                    gp.playSE(1);
                     hasKey++;
                     gp.obj[i] = null;
-                    System.out.println("Key: "+hasKey);
+                    gp.ui.showMessage("You got a key!");
                     break;
                 case "Door":
+                    gp.playSE(3);
                     if(hasKey > 0){
                         gp.obj[i] = null;
                         hasKey--;
+                        gp.ui.showMessage("You opened the door!");
                     }
-                    System.out.println("Key: "+hasKey);
+                    else{
+                        gp.ui.showMessage("You need a key!");
+                    }
+
 
                     break;
+                case "Boots":
+                    gp.playSE(2);
+                    speed += 1;
+                    gp.obj[i] = null;
+                    gp.ui.showMessage("Speed up!");
+                    break;
+                case "Chest":
+                    gp.ui.gameFinished = true;
+                    gp.stopMusic();
+                    gp.playSE(4);
+                    break;
+
             }
         }
     }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -18,16 +18,19 @@ public class GamePanel extends JPanel implements Runnable{
 
     public final int maxWorldCol = 50;
     public final int maxWorldRow = 50;
-    public final int worldWidth = tileSize * maxScreenCol;
-    public final int worldHeight = tileSize * maxWorldRow;
 
     int FPS = 60;
 
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
-    Thread gameThread;
+    Sound se = new Sound();
+    Sound music = new Sound();
+
     public CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
+    public UI ui = new UI(this);
+    Thread gameThread;
+
     public Player player = new Player(this, keyH);
     public SuperObject obj[] = new SuperObject[10];
 
@@ -43,6 +46,8 @@ public class GamePanel extends JPanel implements Runnable{
     public void setupGame(){
 
         aSetter.setObject();
+
+        playMusic(0);
     }
 
     public void startGameThread(){
@@ -127,6 +132,22 @@ public class GamePanel extends JPanel implements Runnable{
         }
 
         player.draw(g2);
+
+        ui.draw(g2);
+
         g2.dispose();
+    }
+
+    public void playMusic(int i){
+        music.setFile(i);
+        music.play();
+        music.loop();
+    }
+    public void stopMusic(){
+        music.stop();
+    }
+    public void playSE(int i){
+        se.setFile(i);
+        se.play();
     }
 }
